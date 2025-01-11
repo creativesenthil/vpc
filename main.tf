f500f17e6e6e3fc3358523cfa9c9f4e100092213
root <root@ip-172-31-13-162.ap-south-1.compute.internal>
2025-01-11 18:49:03 +0000

    vpc

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "MySimpleVPC"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "SimplePublicSubnet"
+  }
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
