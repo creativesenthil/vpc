ea860af10379034b985fb7b94815467679fb5187
creativesenthil <154957462+creativesenthil@users.noreply.github.com>
2025-01-13 22:26:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "ap-south-1"  # Specify the AWS region
+  region = "us-east-1"
 }
 
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"  # CIDR block for the VPC
+  cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
   tags = {
@@ -13,8 +13,8 @@ resource "aws_vpc" "my_vpc" {
 
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"  # Subnet CIDR block
-  availability_zone = "ap-south-1"    # Specify the availability zone
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1"
   tags = {
     Name = "simple-subnet"
   }
